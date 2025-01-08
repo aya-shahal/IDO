54cd8a7731054cf2c0b9cdaba45debf7d34a2fd5
aya-shahal <ayashahal.inventus@gmail.com>
2025-01-08 13:03:49 +0200

    fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./screens/Login/LoginScreen";
 import Dashboard from "./screens/Dashboard/DashboardScreen";
 
 function App() {
-
   const isAuthenticated = !!sessionStorage.getItem("token");
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={isAuthenticated ? <Navigate to="/dashboard" /> : <Login />} />
-        <Route
-          path="/dashboard"
-          element={isAuthenticated ? <Dashboard /> : <Navigate to="/" />}
+        <Route 
+          path="/" 
+          element={isAuthenticated ? <Navigate to="/dashboard" /> : <Login />} 
+        />
+        <Route 
+          path="/dashboard" 
+          element={isAuthenticated ? <Dashboard /> : <Navigate to="/" />} 
         />
       </Routes>
     </Router>
